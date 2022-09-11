079eb69335d5986fe46ad9ada0034d298e7ad273
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2022-09-11 19:58:42 -0300

    criação do Array

---
+++ src/data/Items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Items";
+
+export const items: Item[] = [
+    { date: new Date(2022, 09, 11), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 09, 15), category: 'food', title: 'Burguer King', value: 23.57 },
+    { date: new Date(2022, 09, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2022, 10, 18), category: 'salary', title: 'Salário tecla T', value: 4500 },
+];
